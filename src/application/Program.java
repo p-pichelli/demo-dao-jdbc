8359cbc8770044724c6dd350e70b098b33b3ff73
unknown <pedropichelli04@gmail.com>
2025-06-13 10:50:03 -0300

    Update implemented

--- src/application/Program.java
+++ src/application/Program.java
@@ -36,6 +36,12 @@ public class Program {
 		Seller seller1 = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, dep);
 		sellerDao.insert(seller1);
 		System.out.println("Inserted! New id =" + seller1.getId());
+		
+		System.out.println("=== TESTE 5: update");
+		seller = sellerDao.findById(1);
+		seller.setName("Jorge Carlos");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 	}
 
 }
